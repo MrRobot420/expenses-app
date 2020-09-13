b475a16f60393656d0e817096b67447005c61b2c
MrRobot420 <max.karlchen@web.de>
2020-09-13 13:23:11 +0200

    add modalView WIP

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
-import 'package:expenses/widgets/user_transactions.dart';
+import 'package:expenses/widgets/new_transactions.dart';
+import 'package:expenses/widgets/transaction_list.dart';
 import 'package:flutter/material.dart';
+
+import 'models/transaction.dart';
 
 void main() => runApp(ExpensesApp());
 
@@ -13,31 +16,92 @@ class ExpensesApp extends StatelessWidget {
   }
 }
 
-class TransactionPage extends StatelessWidget {
+class TransactionPage extends StatefulWidget {
+  @override
+  _TransactionPageState createState() => _TransactionPageState();
+}
+
+class _TransactionPageState extends State<TransactionPage> {
+  final List<Transaction> _userTransactions = [
+    Transaction(
+      id: 't1',
+      title: 'Neue Schuhe',
+      amount: 59.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Sixpack Wasser',
+      amount: 2.99,
+      date: DateTime.now(),
+    ),
+  ];
+
+  void _addNewTransaction(String txTitle, double txAmount) {
+    final newTx = Transaction(
+      id: DateTime.now().toString(),
+      title: txTitle,
+      amount: txAmount,
+      date: DateTime.now(),
+    );
+
+    setState(() {
+      _userTransactions.add(newTx);
+    });
+  }
+
+  void _startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+      context: ctx,
+      builder: (_) {
+        return NewTransaction(_addNewTransaction);
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.purple,
-          title: Text('Expenses App'),
-        ),
-        body: SingleChildScrollView(
-                  child: Column(
-            // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Container(
-                width: double.infinity,
-                height: 100,
-                child: Card(
-                  color: Colors.yellow,
-                  child: Text('CHART'),
-                  elevation: 10,
-                ),
+      appBar: AppBar(
+        backgroundColor: Colors.purple,
+        title: Text('Expenses App'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.add_circle,
+              color: Colors.yellow,
+            ),
+            onPressed: () => _startAddNewTransaction(context),
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              height: 100,
+              child: Card(
+                color: Colors.yellow,
+                child: Text('CHART'),
+                elevation: 10,
               ),
-              UserTransactions()
-            ],
-          ),
-        ));
+            ),
+            TransactionList(_userTransactions),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+          Icons.add_circle,
+          color: Colors.yellow,
+        ),
+        onPressed: () => _startAddNewTransaction(context),
+        backgroundColor: Colors.purple,
+      ),
+    );
   }
 }
